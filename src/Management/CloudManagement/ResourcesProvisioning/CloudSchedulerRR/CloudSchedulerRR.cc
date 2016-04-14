956c4f94e3b13590346ace2029e468aede895260
Znikdel <nikdel@gmail.com>
2016-04-14 11:39:02 -0700

    test RR

--- src/Management/CloudManagement/ResourcesProvisioning/CloudSchedulerRR/CloudSchedulerRR.cc
+++ src/Management/CloudManagement/ResourcesProvisioning/CloudSchedulerRR/CloudSchedulerRR.cc
@@ -133,7 +133,7 @@ void CloudSchedulerRR::schedule (){
                            /*
                             * else
                             * {
-                            *
+                            * call timer
                             * }
                             */
 
