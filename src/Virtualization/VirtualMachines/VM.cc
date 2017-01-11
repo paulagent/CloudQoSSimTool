97a424c9531651f70f2418fcc1a54eae4a77dbd5
ZNikdel <nikdel@gmail.com>
2017-01-11 10:40:08 -0800

    small changes

--- src/Virtualization/VirtualMachines/VM.cc
+++ src/Virtualization/VirtualMachines/VM.cc
@@ -167,11 +167,11 @@ void VM::sleep(string containerID){
 
         dockerDaemon->unPauseDockerContainer(containerID);
     }
-   void send_tcp_msg(string msg, string dest_ip)
+   void VM::send_tcp_msg(string msg, string dest_ip)
    {
 
    }
-   void receive_tcp_msg()
+   void VM::receive_tcp_msg()
    {
 
    }
